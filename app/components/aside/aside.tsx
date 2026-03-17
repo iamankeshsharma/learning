265e065919171b7c1139c0fea4ba94b11a70d282
ANKES <ankeshsharma@protonmail.com>
2026-03-17 15:13:38 +0530

    feat: show content on each page

--- app/components/aside/aside.tsx
+++ app/components/aside/aside.tsx
@@ -6,7 +6,7 @@ export function Aside(){
             <nav className="w-fit h-screen flex flex-col justify-start align-center p-5 border-neutral-200 border-r-1">
                 {
                     navLinks?.map(
-                        link => <Link to={link?.href}>{link?.text}</Link>
+                        (link, i) => <Link key={i} to={link?.href}>{link?.text}</Link>
                     )
                 }
             </nav>
--- app/routes/projects.tsx
+++ app/routes/projects.tsx
@@ -1,3 +1,3 @@
-export function Projects(){
+export default function Projects(){
     return <h1>Projects</h1>;
 };
