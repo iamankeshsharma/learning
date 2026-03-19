994736981cadd9a04613f1991e3b5fed1345db01
ANKES <ankeshsharma@protonmail.com>
2026-03-19 15:26:54 +0530

    feat: add clientLoader in about to fetch about data

--- app/routes/about.tsx
+++ app/routes/about.tsx
@@ -1,5 +1,22 @@
-export default function About(){
+import type { Route } from './+types/about';
+
+type loaderDataType = {
+    loaderData:{
+        name: string;
+    description:string;
+    version:string;
+    }
+};
+
+export async function clientLoader(){
+    const data = await fetch(`${import.meta.env.VITE_API_BASE}/about`);
+    return data.json();
+}
+
+export default function About({loaderData}: loaderDataType){
     return <h1>
-        About
+        {Object.keys(loaderData)?.map((i:string)=>{
+            return loaderData[i as keyof typeof loaderData];
+        })}
     </h1>;
 }
