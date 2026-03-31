2e2e7e2166ec9e40a1663c1261e86f6a1228d12b
ANKES <ankeshsharma@protonmail.com>
2026-03-31 18:08:21 +0530

    feat: programmatic navigating.

--- app/routes/contact.tsx
+++ app/routes/contact.tsx
@@ -1,5 +1,12 @@
+import { useNavigate } from "react-router-dom";
+import { ROUTES } from "app/constants/routes";
+
 export default function Contact(){
-    return <h1>
-        Contact
-    </h1>;
+    const navigate = useNavigate();
+    return <>
+        <h1>
+            Contact
+        </h1>
+        <button onClick={()=>navigate(ROUTES.about.href)}>Go to About</button>
+    </>;
 }
